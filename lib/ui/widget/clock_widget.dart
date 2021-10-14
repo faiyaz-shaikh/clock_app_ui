611f721f7408a840faa6478f0880290bfa4d76b7
faiyaz-shaikh <89002539+faiyaz-shaikh@users.noreply.github.com>
2021-10-14 13:34:48 +0530

    Implement clock ui

---
+++ lib/ui/widget/clock_widget.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:clock_app/painter/clock_painter.dart';
+import 'package:flutter/material.dart';
+
+class ClockView extends StatefulWidget {
+  @override
+  _ClockViewState createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  void initState() {
+    Timer.periodic(
+      Duration(seconds: 1),
+      (timer) {
+        setState(() {});
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
+      ),
+    );
+  }
+}
